6e1081aeb6cd0688a0dbfbb1c68eed013ae0a343
ukoloff <ukoloff@gmail.com>
2012-10-30 19:23:24 +0600

    Add command line parsing

--- lib/Help.pl
+++ lib/Help.pl
@@ -1,15 +1,29 @@
 print <<EOT;
 $0: A bit improved version of OpenSSL CA.pl
 
-Usage: $::CFG{0} job-file
+Usage: $0 file -c command -t template var=value section::var=value file -t template var=value ...
+ -xxx		short for command=xxx
+ -c xxx		short for command=xxx
+ -t xxx		short for template=xxx
 
-+-Sample job-file--+
-|command=newcrt    |
-|template=WWW      |
-|CN=www.site.com   |
-+------------------+
+e.g.
+#1: $0 -c sign req=user.req template=email
+#2: $0 -newcrt -t WWW CN=www.site.com req::default_bits=2048
+#3: $0 -selfsigned -t WWW -t subjectAltName CN=me AltName::DNS.1=me AltName::DNS.2=me.domain
+or simply:
+#1: $0 file#1
+#2: $0 file#2
+#3: $0 file#3
++-file#1---------+ +-file#2------------+ +-file#3--------------------+
+|command=sign    | |command=newcrt     | |command=selfsigned         |
+|req=user.req    | |template=WWW       | |template.1=WWW             |
+|template=email  | |CN=www.site.com    | |template.2=subjectAltName  |
++----------------+ |[req]              | |CN=me                      |
+                   |default_bits=2048  | |[AltName]                  |
+                   +-------------------+ |DNS.1=<CN>                 |
+                                         |DNS.2=<CN>.domain          |
+                                         +---------------------------+
 EOT
-
+print "Supported commands: ", join(', ', sort map {$_=basename($_); $_=~s/^.*?\.//; $_=~s/\.[^.]*$//; $_} glob('lib/cmd.*.pl')), "\n";
 exit;
-
 1;
--- lib/main.pl
+++ lib/main.pl
@@ -2,19 +2,57 @@
 # Entry point
 #
 
-Lib('Help')	unless 1==@ARGV;
+Lib('Help')	unless @ARGV;
 Lib('CA');
 
-my $t=Template->new;
-my $fh;
-open $fh, '<', $ARGV[0]	or die "Cannot open '$ARGV[0]': $!\n";
-$t->read($fh);
-close $fh;
-
+my $t=getOpt();
 Job($t);
 
 die "Invalid command '$::CFG{command}'!\n"	unless $::CFG{command}=~/^\w+$/;
 
 Lib("cmd.$::CFG{command}");
+
+sub getOpt
+{
+ my $t=Template->new;
+ my $x='';
+ my $N=1000;
+ foreach my $a(@ARGV)
+ {
+  if('t' eq $x)
+  {
+   $t->fromHash({'template.'.$N++=>$a});
+   $x='';
+   next;
+  }
+  if('c' eq $x)
+  {
+   $t->fromHash({command=>$a});
+   $x='';
+   next;
+  }
+  if($a=~'=')
+  {
+   my @X=split '=', $a, 2;
+   $t->fromHash($X[0]=~/^(.*?)::(.*)$/? {$1=>{$2=>$X[1]}} : {$X[0]=>$X[1]});
+   next;
+  }
+  if($a=~/^-/)
+  {
+   $a=~s/^-//;
+   $x=$a, next	if $a=~/^[tc]$/;
+   $t->fromHash({command=>$a});
+   next;
+  }
+  my $fh;
+  open $fh, '<', $a	or die "Cannot open '$a':$!\n";
+  my $t2=Template->new;
+  $t2->read($fh);
+  close($fh);
+  $t2->prepend($t);
+  $t=$t2;
+ }
+ return $t;
+}
 
 1;
