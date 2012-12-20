ac979b9ae7e0dd8c9aafb2198879eb599b0641c7
ukoloff <ukoloff@gmail.com>
2012-12-20 12:53:55 +0600

    Fix SQL schema

--- schema/pub.10.certs.sql
+++ schema/pub.10.certs.sql
@@ -10,3 +10,18 @@ Create Table Certs(
 
 Create Index iRevoke On Certs(Revoke);
 Create Index iIssuer On Certs(Issuer);
+Create Index iCCtime On Certs(ctime);
+
+-- revokeReason (according to RFC 5280):
+-- unspecified (0)
+-- keyCompromise (1)
+-- CACompromise (2)
+-- affiliationChanged (3)
+-- superseded (4)
+-- cessationOfOperation (5)
+-- certificateHold (6)
+-- (value 7 is not used)
+-- removeFromCRL (8)
+-- privilegeWithdrawn (9)
+-- AACompromise (10)
+
