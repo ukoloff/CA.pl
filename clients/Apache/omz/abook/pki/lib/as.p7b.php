144f659c63cdc741004e5ec8830cc7790c89a78e
ukoloff <ukoloff@gmail.com>
2012-12-22 22:01:45 +0600

    Rewrite export to pkcs7 in pure PHP

--- clients/Apache/omz/abook/pki/lib/as.p7b.php
+++ clients/Apache/omz/abook/pki/lib/as.p7b.php
@@ -2,5 +2,30 @@
 Header('Content-Type: application/octet-stream');
 Header('Content-Disposition: attachment; filename="'.$CFG->fileName.'.p7b"');
 
-echo caExec(Array(command=>'pkcs7', n=>$CFG->params->n, chain=>isset($_GET[chain])));
+$t=tempnam('/var/tmp', 'p7b');
+$f=fopen($t, 'w');
+
+$s=$CFG->db->prepare('Select BLOB From Certs Where id=:n');
+$s->bindValue(':n', $CFG->params->n);
+$r=$s->execute()->fetchArray(SQLITE3_NUM);
+fwrite($f, $r[0]);
+
+if(isset($_GET[chain])):
+ unset($Z);
+ $n=$CFG->params->n;
+ $Z[$n]=1;
+ $s=$CFG->db->prepare("Select id, BLOB From Certs Where id=(Select Issuer From Certs Where id=:n)");
+ while(1):
+  $s->bindValue(':n', $n);
+  $r=$s->execute()->fetchArray(SQLITE3_NUM);
+  $n=$r[0];
+  if(!$n) break;
+  if($Z[$n]) break;
+  $Z[$n]=1;
+  fwrite($f, $r[1]);
+ endwhile;
+endif;
+
+passthru("{$CFG->OpenSSL} crl2pkcs7 -nocrl -certfile $t");
+unlink($t);
 ?>
